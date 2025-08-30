e98813d8c8fea8e6c417f25aeeb28389dfe17b1f
Musungu2 <ruthmusungu2019@gmail.com>
2025-08-30 21:06:29 +0300

    updates

--- smartduka_project/urls.py
+++ smartduka_project/urls.py
@@ -10,17 +10,9 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-
-    # Accounts / Authentication routes
     path("users/", include("users.urls")),
-
-    # Inventory routes
     path("inventory/", include("inventory.urls")),
-
-    # Orders routes
     path("orders/", include("orders.urls")),
-
-    # Reports routes
     path("reports/", include("reports.urls")),
 ]
 
